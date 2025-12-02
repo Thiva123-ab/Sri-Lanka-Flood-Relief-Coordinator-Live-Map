641ac2d0e10e3ec1e1924545a87b01ea3977c6b9
Thivanka Tharuka <priyanthagtt@students.nsbm.ac.lk>
2025-12-02 22:26:01 +0530

    Add files via upload
    This is the structure  of our project

---
+++ Sri Lanka_Flood_Relief_Coordinator_and_Live_Map/src/test/java/ac/nsbm/srilanka_flood_relief_coordinator_and_live_map/SriLankaFloodReliefCoordinatorAndLiveMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.nsbm.srilanka_flood_relief_coordinator_and_live_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriLankaFloodReliefCoordinatorAndLiveMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
