73de35191799b3d123995bb396aed8b11ab3598e
Thivanka Tharuka <thivankatharuka36@gmail.com>
2025-12-03 14:34:37 +0530

    Crete repository  package and crete 3 java classes AlertRepository, HelprequestRepository and MapMarkerRepository

---
+++ Sri Lanka_Flood_Relief_Coordinator_and_Live_Map/src/main/java/ac/nsbm/srilanka_flood_relief_coordinator_and_live_map/repository/AlertRepository.java
@@ -0,0 +1,10 @@
+package ac.nsbm.srilanka_flood_relief_coordinator_and_live_map.repository;
+
+import ac.nsbm.srilanka_flood_relief_coordinator_and_live_map.model.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+    // Finds latest alerts first
+    List<Alert> findAllByOrderByTimestampDesc();
+}
---
+++ Sri Lanka_Flood_Relief_Coordinator_and_Live_Map/src/main/java/ac/nsbm/srilanka_flood_relief_coordinator_and_live_map/repository/MapMarkerRepository.java
@@ -0,0 +1,9 @@
+package ac.nsbm.srilanka_flood_relief_coordinator_and_live_map.repository;
+
+import ac.nsbm.srilanka_flood_relief_coordinator_and_live_map.model.MapMarker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MapMarkerRepository extends JpaRepository<MapMarker, Long> {
+    List<MapMarker> findByType(String type);
+}
